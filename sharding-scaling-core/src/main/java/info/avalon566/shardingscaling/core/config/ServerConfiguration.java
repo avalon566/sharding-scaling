72abd40ea916a59792503c100733449af289f854
ssxlulu <623492055@qq.com>
2019-11-09 14:25:13 +0800

    #31 Configuration optimization-add default value

--- sharding-scaling-core/src/main/java/info/avalon566/shardingscaling/core/config/ServerConfiguration.java
+++ sharding-scaling-core/src/main/java/info/avalon566/shardingscaling/core/config/ServerConfiguration.java
@@ -1,18 +1,14 @@
 package info.avalon566.shardingscaling.core.config;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@AllArgsConstructor
-@NoArgsConstructor
 @Data
 public class ServerConfiguration {
 
-    private Integer blockQueueSize;
+    private Integer blockQueueSize = 10000;
 
-    private Integer pushTimeout;
+    private Integer pushTimeout = 1000;
 
-    private Integer concurrency;
+    private Integer concurrency = 3;
 
 }
